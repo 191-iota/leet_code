62eda05bea3da1fbd7e44de2959069405c4d2ef7
LeMoiz <moizm2324@gmail.com>
2024-11-23 14:04:39 +0100

    feat: second attempt TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -3,7 +3,40 @@ import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
+        int[] array =  {-1,-2,-3,-4,-5};
 
+        twoSum(array, -8);
+    }
+
+    // Attempt 2 (doesn't work)
+
+    public static int[] twoSum2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (Math.abs(currentSum) + Math.abs(nums[i]) <= Math.abs(target) || target == 0 && i == 0) {
+                currentSum += nums[i];
+                map.put(i, nums[i]);
+
+            } else {
+                int targetValue = Math.abs(currentSum) + Math.abs(nums[i]) - Math.abs(target);
+
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                    if (Math.abs(entry.getValue()) == targetValue) {
+                        map.remove(entry.getKey());
+                        map.put(i, nums[i]);
+                        break;
+                    } else if(target == 0 && Math.abs(currentSum + nums[i]) == target) {
+                        map.put(i, nums[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return map.keySet().stream().mapToInt(Integer::intValue).toArray();
     }
 
     // Attempt 1 (doesn't work)

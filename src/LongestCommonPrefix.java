835cceb9e6714a77461ddbca2123d2ac1dec7826
Murc13lag0 <moizm2324@gmail.com>
2024-12-27 17:41:07 +0100

    feat: third attempt LongestCommonPrefix

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,7 +1,33 @@
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] test = {"ac","ac","a","a"};
-        longestCommonPrefix2(test);
+        String[] test = {"abab","aba",""};
+        longestCommonPrefix3(test);
+    }
+
+    // Attempt 3 - (works)
+    public static String longestCommonPrefix3(String[] strs) {
+
+        int commonPrefixLength = 0;
+        int shortestLength = strs[0].length();
+        // evaluate the shortest length of a word which can be the longest prefix
+        while (commonPrefixLength < shortestLength) {
+
+            for (int i = 0; i < strs.length - 1; i++) {
+                if (commonPrefixLength == 0 && shortestLength > strs[i + 1].length()) {
+                    shortestLength = strs[i + 1].length();
+                    if(shortestLength == 0){
+                        return "";
+                    }
+                }
+
+                if (strs[i].charAt(commonPrefixLength) != strs[i + 1].charAt(commonPrefixLength)) {
+                    return strs[i].substring(0, commonPrefixLength);
+                }
+            }
+            commonPrefixLength++;
+        }
+
+        return strs[0].substring(0, commonPrefixLength);
     }
 
     // Attempt 2 - (does not work)

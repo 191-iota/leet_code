9cae0da5e21d70bed27d3d491123697bc9ec0568
Murc13lag0 <moizm2324@gmail.com>
2025-01-04 14:23:03 +0100

    feat: third in progress attempt InsertInterval

--- src/InsertInterval.java
+++ src/InsertInterval.java
@@ -1,11 +1,137 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class InsertInterval {
     public static void main(String[] args) {
-        int[][] test = {{1,2},{3,5},{6,7},{8,10},{12,16}};
-        int[] newInterval = {4,8};
-        insert(test, newInterval);
+        int[][] test = {{2,3},{5,7}};
+        int[] newInterval = {0,7};
+        insert2(test, newInterval);
+    }
 
+    // Attempt 3 - in progress
+    // loop through the array
+    // check if new interval is part of interval
+    // if the last number is not part of it or the same as the next interval, then skip this when building an array
+    private static int[][] insert3(int[][] intervals, int[] newInterval) {
+        // case 0: if intervals are empty just return the new interval
+        if(intervals.length == 0) return new int[][]{newInterval};
+        // case 0.1: if new intervals are empty just return intervals
+        if(newInterval.length == 0) return intervals;
+
+
+        List<Integer> result = new ArrayList<>();
+        boolean firstIntervalSet = false;
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][0] <= newInterval[0]) {
+                // case 1: Interval beginning is smaller than new interval beginning
+                result.add(intervals[i][0]);
+
+                // case 1.1: If new interval ending is bigger than current ending but smaller than next beginning
+                if (intervals[i][1] < newInterval[1] && intervals[i + 1][0] < newInterval[1]) {
+                    result.add(newInterval[1]);
+                    firstIntervalSet = true;
+                } else {
+                    result.add(intervals[i][1]);
+                }
+                continue;
+            }
+
+            // case 2: do nothing if interval overlaps new interval else add it
+            if(firstIntervalSet && intervals[i][1] > newInterval[1]) {
+                result.add(intervals[i][0]);
+                result.add(intervals[i][1]);
+
+                // case 2.1: If new interval ending is bigger than current ending but smaller than next beginning
+                if (intervals[i][1] < newInterval[1] && intervals[i + 1][0] < newInterval[1]) {
+                    result.add(newInterval[1]);
+                } else {
+                    result.add(intervals[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i < result.size(); i++) {
+            intervals[i][0] = result.get(i);
+            intervals[i][1] = result.get(i);
+        }
+        return ;
+    }
+
+    // Attempt 2 - does not work
+    private static int[][] insert2(int[][] intervals, int[] newInterval) {
+        if(intervals.length == 0) return new int[][]{newInterval};
+        if(newInterval.length == 0) return intervals;
+        if(intervals[0][1] < newInterval[0] && intervals.length == 1) {
+            int[][] result = new int[intervals.length + 1][2];
+            result[0][0] = intervals[0][0];
+            result[0][1] = intervals[0][1];
+            result[1][0] = newInterval[0];
+            result[1][1] = newInterval[1];
+            return result;
+        }
+
+        int from = -1;
+        int fromIndex = 0;
+        int toIndex = 0;
+        int to = -1;
+        int skipped = -1;
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (from != -1) {
+                skipped++;
+            }
+
+            if(i == 0 && intervals[i][0] > newInterval[0]){
+                from = newInterval[0];
+                fromIndex = i;
+            } else if (intervals[i][1] >= newInterval[0] && from == -1) {
+                from = intervals[i][0];
+                fromIndex = i;
+            }
+            if (intervals[i][1] > newInterval[1] && newInterval[1] >= intervals[i][0]) {
+                to = intervals[i][1];
+                toIndex = i;
+                break;
+            } else if (intervals[i][1] > newInterval[1] && intervals[i][0] >= newInterval[1]) {
+
+                to = newInterval[1];
+                toIndex = i - 1;
+                break;
+            } else if(intervals[i][1] < newInterval[1] && intervals.length == 1) {
+                to = newInterval[1];
+                toIndex = i;
+            }
+        }
+
+        if(skipped > 0){
+            skipped++;
+        } else if(skipped == -1){
+            skipped = 0;
+        }
+
+        int[][] result = new int[intervals.length - skipped][2];
+
+        // Build the array
+        for (int i = 0; i < result.length; i++) {
+
+            if(i == fromIndex && from != -1) {
+                result[i][0] = from;
+            }
+
+            if(i + skipped == toIndex && from != -1) {
+                result[i][1] = to;
+            } else if(i > toIndex - skipped) {
+                result[i][0] = intervals[i + skipped][0];
+                result[i][1] = intervals[i + skipped][1];
+            } else {
+                result[i][0] = intervals[i][0];
+                result[i][1] = intervals[i][1];
+            }
+        }
+        System.out.println(Arrays.deepToString((result)));
+        return result;
     }
 
     // Attempt 1 (in progress)

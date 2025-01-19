d522f4c15bfeee51bd2d95b1fc3245a452ac2f3c
Murc13lag0 <moizm2324@gmail.com>
2025-01-19 14:57:41 +0100

    feat: optimize repeatedSubstringPattern problem

---
+++ src/DivideTwoIntegers.java
@@ -0,0 +1,12 @@
+public class DivideTwoIntegers {
+    public static void main(String[] args) {
+        
+    }
+
+    
+
+
+
+    
+    
+}
--- src/RepeatedSubstringPattern.java
+++ src/RepeatedSubstringPattern.java
@@ -1,11 +1,45 @@
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
 public class RepeatedSubstringPattern {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+  }
+
+  private static boolean repeatedSubstringPattern2o(String s) {
+    for (int i = 1; i <= s.length() / 2; i++) {
+      if (s.length() % i == 0 && s.substring(0, i).repeat(s.length() / i).equals(s)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // attempt 2
+  private static boolean repeatedSubstringPattern2(String s) {
+    StringBuilder sb = new StringBuilder();
+
+    for (short i = 2; i <= s.length() / 2; i++) {
+
+      if (s.length() % i == 0) {
+        while (sb.length() < s.length()) {
+          sb.append(s, 0, i);
+        }
+      } else {
+        continue;
+      }
+
+      if (s.contentEquals(sb)) {
+        return true;
+      }
+      sb.setLength(0);
+    }
+    return false;
+  }
 
   // attempt 1 - does not work
-  public static boolean repeatedSubstringPattern(String s) {
+  private static boolean repeatedSubstringPattern(String s) {
     // this will be the repeated pattern
     StringBuilder sb = new StringBuilder();
     Set<Character> set = new HashSet<>();

b5024c26ce0d3c3c7a91d5551b0ef0eca2896be6
Murc13lag0 <moizm2324@gmail.com>
2025-01-27 16:09:10 +0100

    feat: second and third attempt addbinary

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -6,7 +6,76 @@ public class AddBinary {
         
     }
     
-    
+    // attempt 3 - optimized
+    public static String addBinary3(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        int carry = 0;
+        int maxLength = Math.max(a.length(), b.length());
+
+        for (int i = maxLength - 1; i >= 0; i--) {
+            int currentSum = carry;
+            int aIndex = i - (maxLength - a.length());
+            int bIndex = i - (maxLength - b.length());
+
+            if (aIndex >= 0) {
+                currentSum += a.charAt(aIndex) - '0';
+            }
+            if (bIndex >= 0) {
+                currentSum += b.charAt(bIndex) - '0';
+            }
+            result.append(currentSum % 2);
+
+            carry = currentSum / 2;
+        }
+
+        if(carry > 0) {
+            result.append(carry);
+        }
+
+        result.reverse();
+
+        return result.toString();
+    }
+
+    // attempt 2 - in works but is also slow
+    public static String addBinary2(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        String padding;
+        // add padding to make them the same length
+        if (a.length() > b.length()) {
+            padding = " ".repeat(a.length() -  b.length());
+            b = padding + b;
+        } else {
+            padding = " ".repeat(b.length() -  a.length());
+            a = padding + a;
+        }
+
+        int carry = 0;
+        // both are the same size anyways
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int currentSum = carry;
+
+            if (Character.isDigit(a.charAt(i))) {
+                currentSum += Integer.parseInt(String.valueOf(a.charAt(i)));
+
+                if (Character.isDigit(b.charAt(i))) {
+                    currentSum += Integer.parseInt(String.valueOf(b.charAt(i)));
+                }
+
+                result.append(currentSum % 2);
+                carry = currentSum / 2;
+            }
+        }
+        if(carry != 0) {
+            result.append(carry);
+        }
+        result.reverse();
+
+        return result.toString();
+    }   
+
     // attempt 1 - works but it is slow 
     public static String addBinary(String a, String b) {
         BigInteger bigA = new BigInteger(a, 2);

225a6f6d622890564f9b114150c291a03c92dbbb
Murc13lag0 <moizm2324@gmail.com>
2025-05-26 09:51:49 +0200

    start 3sum

---
+++ src/Rust/three_sum/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    // fixed pointer
+    let mut i = nums[0];
+
+    for k in nums.iter().enumerate() {
+        for j in nums.iter().enumerate() {}
+    }
+}

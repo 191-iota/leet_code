20936916522c21b37978ce229c9796120c4c120f
Murc13lag0 <moizm2324@gmail.com>
2025-06-14 12:30:12 +0000

    current progress

--- src/Rust/longest_palindrome_substring/src/main.rs
+++ src/Rust/longest_palindrome_substring/src/main.rs
@@ -1,23 +1,30 @@
-use std::collections::BinaryHeap;
-
 fn main() {
     println!("Hello, world!");
 }
 
+// attempt 1 - does not work
 pub fn longest_palindrome(s: String) -> String {
-    let mut longest: &str = "";
+    if s.len() == 1 {
+        return s;
+    }
+
+    let mut longest_range: (i32, i32) = (0, 0);
     let split_string: Vec<char> = s.chars().collect();
 
-    for i in 0..split_string.len() {
-        for j in (0..(split_string.len() / 2)).rev() {
-            let mut ptr = i;
+    let mut ptr;
+    if split_string.len() % 2 == 0 {
+        ptr = (split_string.len() / 2) + 1;
+    } else {
+        ptr = split_string.len() / 2;
+    }
 
-            if split_string[ptr] != split_string[split_string.len() - 1 - j] {
-                break;
-            } else if j == split_string.len() / 2 {
-                longest = &[]
-            }
+    for i in (0..(split_string.len() / 2)).rev() {
+        if i > 0 && split_string[ptr] != split_string[i] {
+            longest_range = (i as i32 + 1, ptr as i32 - 1);
+            ptr += 1;
         }
     }
-    longest.to_string()
+    split_string[longest_range.0 as usize..longest_range.1 as usize]
+        .iter()
+        .collect()
 }

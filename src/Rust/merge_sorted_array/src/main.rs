a7231939a6c0fd8e7e191cc3cc9087c093b02be1
Murc13lag0 <moizm2324@gmail.com>
2025-06-18 09:45:37 +0000

    merge two sorted arrays

---
+++ src/Rust/merge_sorted_array/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    let mut sorted: Vec<i32> = Vec::new();
+    let mut last_m: i32 = 0;
+    let mut last_n: i32 = 0;
+
+    if m == 0 {
+        for i in 0..n {
+            nums1[i as usize] = nums2[i as usize];
+        }
+        return;
+    } else if n == 0 {
+        return;
+    }
+
+    while last_m + last_n < (m + n) {
+        if last_m < m && last_n < n && nums1[last_m as usize] <= nums2[last_n as usize] {
+            sorted.push(nums1[last_m as usize]);
+            last_m += 1;
+        } else if last_n < n && last_m < m && nums1[last_m as usize] >= nums2[last_n as usize] {
+            sorted.push(nums2[last_n as usize]);
+            last_n += 1;
+        } else if last_n < n && last_m == m {
+            sorted.push(nums2[last_n as usize]);
+            last_n += 1;
+        } else if last_n == n && last_m < m {
+            sorted.push(nums1[last_m as usize]);
+            last_m += 1;
+        }
+    }
+
+    for i in 0..(m + n) {
+        nums1[i as usize] = sorted[i as usize];
+    }
+}

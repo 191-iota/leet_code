c36c88a977e09874c8dad1456b1d552af80ee35c
Murc13lag0 <moizm2324@gmail.com>
2025-07-13 21:02:50 +0200

    zigzag conversion

--- src/Rust/zigzag_conversion/src/main.rs
+++ src/Rust/zigzag_conversion/src/main.rs
@@ -3,25 +3,33 @@ fn main() {
 }
 
 pub fn convert(s: String, num_rows: i32) -> String {
-    // loop through the string
-    // write vertically num rows
-    // when reached continue by \n and going up one line till
-    let mut char = s.chars();
-
-    let mut out = String::new();
-
-    let mut counter = 0;
-    let mut row_loop = true;
-
-    while let Some(v) = char.next() {
-        while counter % num_rows != 0 && row_loop {
-            out.push(v);
-            out.push('\n');
-            char.next();
-            counter += 1;
-        }
-
-        row_loop = true;
+    if num_rows == 1 || num_rows as usize >= s.len() {
+        return s;
     }
-    out
+
+    let mut rows = vec![String::new(); num_rows as usize];
+    let mut row = 0;
+    let mut down = true;
+
+    let mut chars = s.chars();
+
+    while let Some(c) = chars.next() {
+        rows[row].push(c);
+
+        if down {
+            if row + 1 < num_rows as usize {
+                row += 1;
+            } else {
+                down = false;
+                row -= 1;
+            }
+        } else if row > 0 {
+            row -= 1;
+        } else {
+            down = true;
+            row += 1;
+        }
+    }
+
+    rows.concat()
 }

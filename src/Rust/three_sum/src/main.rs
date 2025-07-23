8e4d66584c843f294110eafbda041aa6ed37c47b
Murc13lag0 <moizm2324@gmail.com>
2025-07-23 22:58:44 +0200

    three sum

--- src/Rust/three_sum/src/main.rs
+++ src/Rust/three_sum/src/main.rs
@@ -1,12 +1,48 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("Hello, world!");
 }
 
 pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     // fixed pointer
-    let mut i = nums[0];
+    let mut sorted = nums.clone();
+    sorted.sort();
 
-    for k in nums.iter().enumerate() {
-        for j in nums.iter().enumerate() {}
+    let mut res: Vec<Vec<i32>> = Vec::new();
+    let mut set: HashSet<Vec<i32>> = HashSet::new();
+    let mut ptr1 = 0;
+    let mut ptr2 = sorted.len() - 1;
+
+    // We set a baseline number which is the for loop enummeration and add two sum on top of that number
+    for (i, v) in sorted.iter().enumerate() {
+        let baseline = v;
+        if i > 0 && sorted[i] == sorted[i - 1] {
+            continue;
+        }
+        ptr1 = i + 1;
+        ptr2 = sorted.len() - 1;
+        while ptr1 < ptr2 {
+            if baseline + sorted[ptr1] + sorted[ptr2] == 0 {
+                let curr = vec![*baseline, sorted[ptr1], sorted[ptr2]];
+                if !set.contains(&curr) {
+                    res.push(vec![*baseline, sorted[ptr1], sorted[ptr2]]);
+                    set.insert(curr);
+                }
+                ptr1 += 1;
+                ptr2 -= 1;
+            } else if baseline + sorted[ptr1] + sorted[ptr2] < 0 {
+                if ptr1 >= sorted.len() - 1 {
+                    break;
+                }
+                ptr1 += 1;
+            } else {
+                if ptr2 <= 0 {
+                    break;
+                }
+                ptr2 -= 1;
+            }
+        }
     }
+    res
 }
--- src/Rust/two_sum_ii/src/main.rs
+++ src/Rust/two_sum_ii/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut ptr1 = 0;
+    let mut ptr2 = numbers.len() - 1;
+
+    loop {
+        if numbers[ptr1] + numbers[ptr2] == target {
+            return vec![(ptr1 + 1) as i32, (ptr2 + 1) as i32];
+        } else if numbers[ptr1] + numbers[ptr2] < target {
+            ptr1 += 1;
+        } else {
+            ptr2 -= 1;
+        }
+    }
+}

2f81892b52c0c1a4ad6c0d7d7524489ffecb8450
Murc13lag0 <moizm2324@gmail.com>
2025-08-09 20:55:55 +0200

    current progress

--- src/Rust/minimum_window/src/main.rs
+++ src/Rust/minimum_window/src/main.rs
@@ -11,9 +11,10 @@ pub fn min_window(s: String, t: String) -> String {
     if s.len() < t.len() {
         return "".to_string();
     }
+
     let mut ideal = HashMap::with_capacity(t.len());
     let mut curr = HashMap::new();
-    let mut ranges: (usize, usize) = (0, s.len() - 1);
+    let mut ranges: (usize, usize) = (0, s.len());
     let chars: Vec<char> = s.chars().collect();
     for s in t.chars() {
         *ideal.entry(s).or_insert(0) += 1;
@@ -21,39 +22,51 @@ pub fn min_window(s: String, t: String) -> String {
 
     let mut matching_keys = 0;
     let mut start_bound: usize = 0;
-    for (i, s) in chars.iter().enumerate() {
-        if ideal.contains_key(s) {
-            *curr.entry(s).or_insert(0) += 1;
-            if ideal.get(s).unwrap() == curr.get(s).unwrap() {
-                matching_keys += 1;
-            }
+    let mut end_bound: usize = 0;
 
-            if matching_keys == ideal.len() && (ranges.1 - ranges.0) > (i - start_bound) {
-                ranges = (start_bound, i);
-            }
+    while start_bound < chars.len() && end_bound < chars.len() {
+        for (i, s) in chars.iter().enumerate().skip(end_bound) {
+            if ideal.contains_key(s) {
+                *curr.entry(s).or_insert(0) += 1;
 
-            // make window smaller left to right
-            loop {
-                let current_char = &chars[start_bound];
-                if curr.contains_key(current_char) {
-                    let count = *curr.get(current_char).unwrap();
-                    if let Some(v) = curr.get_mut(&chars[start_bound]) {
-                        if *ideal.get(current_char).unwrap() == count {
-                            if matching_keys == ideal.len()
-                                && (ranges.1 - ranges.0) > (i - start_bound)
-                            {
-                                ranges = (start_bound, i);
-                            }
-                            *v -= 1;
-                            matching_keys -= 1;
-                            start_bound += 1;
-                            break;
-                        }
-                    }
+                if ideal.get(s).unwrap() == curr.get(s).unwrap() {
+                    matching_keys += 1;
                 }
-                start_bound += 1;
+
+                end_bound = i + 1;
+
+                if matching_keys == ideal.len() {
+                    if (i + 1 - start_bound) < (ranges.1 - ranges.0) {
+                        ranges = (start_bound, i + 1);
+                    }
+                    break;
+                }
             }
         }
+
+        // make window smaller left to right
+        while start_bound < chars.len() && end_bound < chars.len() {
+            let current_char = &chars[start_bound];
+            if ideal.contains_key(current_char) && curr.contains_key(current_char) {
+                let count = *curr.get(current_char).unwrap();
+                if let Some(v) = curr.get_mut(current_char) {
+                    if *ideal.get(current_char).unwrap() == count {
+                        if matching_keys == ideal.len()
+                            && (ranges.1 - ranges.0) > (end_bound - start_bound)
+                        {
+                            println!("{}, {}", ranges.0, ranges.1);
+                            ranges = (start_bound, end_bound);
+                        }
+                        *v -= 1;
+                        matching_keys -= 1;
+                        start_bound += 1;
+                        break;
+                    }
+                }
+            }
+            start_bound += 1;
+        }
     }
-    s[ranges.0..=ranges.1].to_string()
+
+    s[ranges.0..ranges.1].to_string()
 }
